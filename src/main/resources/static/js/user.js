7754e00b7fbc831e79206144a2543a63872f5b4b
310-29 <310-29@DESKTOP-NMAS9PK>
2023-06-01 13:55:21 +0900

    jquery 회원가입

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,36 +1,76 @@
 /**
  * 
  */
- function usersubmit() {
-	alert("회원가입 요청됨:old style javascript");
+ /*function usersubmit() {
+   alert("회원가입 요청됨:old style javascript");
 
-	let user = {
-		username: document.getElementById("username").value,
-		password: document.getElementById("password").value,
-		email: document.getElementById("email").value
-	}
-	console.log(user);
+   let user = {
+      username: document.getElementById("username").value,
+      password: document.getElementById("password").value,
+      email: document.getElementById("email").value
+   }
+   console.log(user);
 
-/* http request 
-*/
-	var ajaxrequest = new XMLHttpRequest();
+   var ajaxrequest = new XMLHttpRequest();
 
-	ajaxrequest.open('POST', '/user');
-	JSON.stringify(user);
-	ajaxrequest.onreadystatechange = function() {
-		if (ajaxrequest.readyState == 4) {
-			response = ajaxrequest.responseText;
-			alert(response);
-			location - "/";
-		}
-	}
-	ajaxrequest.setRequestHeader("Content-Type", "application/json");
-	ajaxrequest.send(JSON.stringify(user));
+   ajaxrequest.open('POST', '/user');
+   JSON.stringify(user);
+   ajaxrequest.onreadystatechange = function() {
+      if (ajaxrequest.readyState == 4) {
+         response = ajaxrequest.responseText;
+         alert(response);
+         location - "/";
+      }
+   }
+   ajaxrequest.setRequestHeader("Content-Type", "application/json");
+   ajaxrequest.send(JSON.stringify(user));
 }
 
 function memberjoin() {
-	let btnsave = document.getElementById("btn-save");
-	btnsave.onclick = usersubmit;
+   let btnsave = document.getElementById("btn-save");
+   btnsave.onclick = usersubmit;
 }
 
-memberjoin();
+memberjoin();*/
+
+/**
+ /sbootblog/src/main/resources/static/js/user.js
+ */
+
+ let userObject = {
+    init: function(){
+        let _this = this;
+
+        $("#btn-save").on("click",() =>{
+            _this.insertUser();
+        });
+    },
+
+ insertUser: function() {
+      alert("회원가입 요청됨");
+      // 회원정보 추출
+      let user = {
+         username: $("#username").val(),
+         password: $("#password").val(),
+         email: $("#email").val()
+      }
+      
+      console.log(user);
+
+      $.ajax({
+         type: "POST",
+         // 시험으로는 /user로 함
+         url: "/auth/insertUser",
+         data: JSON.stringify(user),
+         contentType: "application/json; charset=utf-8"
+      }).done(function(response) {
+         console.log(response);
+         alert(response);
+         location = "/";
+      }).fail(function(error) {
+         alert("!/js/user.js에서 에러발생: " + error);
+      });
+   },
+}
+
+ userObject.init();
